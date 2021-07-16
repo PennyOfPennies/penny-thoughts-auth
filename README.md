379dd058adbdf2166ef0dbbb8afb009ea0501ed6
lividlair <lividlair@Ryans-MacBook-Pro.local>
2021-07-16 17:56:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# penny-thoughts-auth
---
+++ action/index.js
@@ -0,0 +1,14 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+async function run() {
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+	const { context = {} } = github;
+	// const { pull_request } = context.payload;
+
+	const exists = GITHUB_TOKEN ? "Token exists" : "Does not exist";
+	
+	console.log("Hello, world!", exists, context);
+}
+
+run();
