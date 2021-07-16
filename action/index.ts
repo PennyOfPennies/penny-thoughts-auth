4b2f8bd0ce2a2de35c9baee4ba09e881b43c71ab
lividlair <lividlair@Ryans-MacBook-Pro.local>
2021-07-16 18:48:46 -0400

    secret test

--- action/index.ts
+++ action/index.ts
@@ -3,12 +3,15 @@ import * as github from '@actions/github';
 
 async function run() {
 	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-	const { context = {} } = github;
+	const TEST_SECRET = core.getInput("TEST_SECRET");
+	// const { context = {} } = github;
 	// const { pull_request } = context.payload;
 
-	const exists = GITHUB_TOKEN ? "Token exists" : "Does not exist";
+	// const exists = GITHUB_TOKEN ? "Token exists" : "Does not exist";
 	
-	console.log("Hello, world!", exists, context);
+	// console.log("Hello, world!", exists, context);
+	console.log("A Test", "JUST_A_SECRET")
+	console.log("A Test", TEST_SECRET)
 }
 
 run();
